c9912f001411892514db4bbca7263a3a84ac9569
Phillip Smith <ps@phillipadsmith.com>
2014-12-15 15:30:57 -0800

    Quick first pass at the character map

---
+++ ui/js/character-map.js
@@ -0,0 +1,82 @@
+window.App = {};
+App.public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1OdhbHZ8jnGZ4lvUAfNrgGCZjpnWcaVuFhtJp3LR6qps/pubhtml';
+App.storage = Tabletop.init( { key: App.public_spreadsheet_url, wait: true } );
+App.Character = Backbone.Model.extend({
+    idAttribute: 'name',
+    defaults: {
+        "image": ""  
+    },
+    tabletop: {
+        instance: App.storage,
+        sheet: 'Sheet1'
+    },
+    sync: Backbone.tabletopSync,
+    initialize: function(){
+        this.checkImage();
+    },
+    checkImage: function() {
+        var self = this;
+        var name = self.get('name');
+        var gender = self.get('gender');
+        $.ajax({
+            url:'/ui/img/' + name + '-small.jpg',
+            type:'HEAD',
+            error: function()
+            {
+                var person = App.characters.get(name);
+                person.set('image','default-' + gender );
+                //file not exists
+            },
+            success: function()
+            {
+                //file exists
+                var person = App.characters.get(name);
+                person.set('image', name );
+            }
+        });
+    }
+});
+App.CharacterCollection = Backbone.Collection.extend({
+    // Reference to this collection's model.
+    model: App.Character,
+    tabletop: {
+        instance: App.storage,
+        sheet: 'Sheet1'
+    },
+    sync: Backbone.tabletopSync
+});
+App.CharacterView = Backbone.View.extend({
+    tagname: 'div',
+    template: _.template($('#person-template').html()),
+    render: function() {
+        $(this.el).html(this.template(this.model.toJSON()));
+        return this;
+    },
+    initialize: function() {
+        this.listenTo(this.model, "change", this.render);
+    },
+});
+App.CharactersView = Backbone.View.extend({
+    tagname: 'div',
+    template: _.template($('#people-template').html()),
+    render: function() {
+        $(this.el).html(this.template({ characters: this.collection.toJSON() }));
+        return this;
+    },
+    events: {
+        "mouseover .person-photo": "showCharacter",
+        "click .person-photo": "showCharacter"
+    },
+    initialize: function() {
+        this.listenTo(this.collection, "change", this.render);
+    },
+    showCharacter: function(event) {
+        if ( event.currentTarget.dataset.person ) {
+            var person = event.currentTarget.dataset.person;
+            App.person_view = new App.CharacterView({ model: App.characters.get(person) });
+            $("#character-spotlight div").remove();
+            $("#character-spotlight").append( App.person_view.render().el );
+        }
+
+    }
+});
